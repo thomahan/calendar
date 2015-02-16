c1dc20cad6161be02cbe4732fdefac988b281a56
thomahan <thomasrenehansen@gmail.com>
2015-02-16 11:44:33 +0100

    Opprettet fil og la til MySQL-innloggingsinfo

---
+++ src/db/DBConnector.java
@@ -0,0 +1,7 @@
+package db;
+
+public class DBConnector {
+	private static String url = "jdbc:mysql://mysql.stud.ntnu.no/thomahan_pu_g41_kalender";
+	private static String username = "thomahan_pu_g41";
+	private static String password = "g41aahmt";
+}
