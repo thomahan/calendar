40a533c0d679315533084e1423f2cd2b6654a5e0
thomahan <thomasrenehansen@gmail.com>
2015-02-23 17:35:51 +0100

    Add tentative salt method

--- src/main/PasswordHash.java
+++ src/main/PasswordHash.java
@@ -4,8 +4,17 @@ import java.security.MessageDigest;
 import java.security.SecureRandom;
 
 public class PasswordHash {
-	public static String getSalt() {
-		// TODO
+	private static final int SALT_BYTE_SIZE = 256;
+
+	/**
+	 * Creates a salt of random bytes to be hashed together with the password
+	 * @return byte[]
+	 */
+	public static byte[] getSalt() {
+		byte[] randomBytes = new byte[SALT_BYTE_SIZE];
+		SecureRandom secrand = new SecureRandom();
+		secrand.nextBytes(randomBytes);
+		return randomBytes;
 	}
 
 }
