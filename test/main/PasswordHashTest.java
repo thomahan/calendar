7c9fdc5266e85b1591af87df2186ac4d3bb555d1
thomahan <thomasrenehansen@gmail.com>
2015-02-23 20:14:53 +0100

    Add tests for salt uniqueness and hashing consistency

--- test/main/PasswordHashTest.java
+++ test/main/PasswordHashTest.java
@@ -1,17 +1,40 @@
 package main;
 
 import org.junit.Test;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 
 import main.PasswordHash;
 
 public class PasswordHashTest {
+	@Test
 	public void getSaltShouldReturnDifferentSalts() {
-		byte[] saltA = PasswordHash.getSalt();
-		byte[] saltB = PasswordHash.getSalt();
+		String saltA = PasswordHash.getSalt();
+		String saltB = PasswordHash.getSalt();
+		
+		/*
 		System.out.println(saltA);
+		System.out.println(saltB);
+		//*/
 		
 		assertFalse(saltA.equals(saltB));
 	}
+	
+	@Test
+	public void hashingShouldBeConsistent() {
+		String salt = PasswordHash.getSalt();
+		String password = "abc123";
+		String hashA = PasswordHash.getPasswordHash(salt, password);
+		String hashB = PasswordHash.getPasswordHash(salt, password);
 
+		/*
+		System.out.println(salt);
+		System.out.println(password);
+		System.out.println(hashA);
+		System.out.println(hashB);
+		System.out.println(hashB.length());
+		//*/
+		
+		assertTrue(hashA.equals(hashB));
+	}
 }
