110f31aeba4f4fb5305890b770ada263098396f0
thomahan <thomasrenehansen@gmail.com>
2015-02-23 14:08:04 +0100

    Add test for connection to database

--- test/db/DBConnectorTest.java
+++ test/db/DBConnectorTest.java
@@ -1,7 +1,14 @@
 package db;
 
 import db.DBConnector;
+import java.sql.ResultSet;
+import org.junit.Test;
+import static org.junit.Assert.assertFalse;
+
 
 public class DBConnectorTest {
-
+	public void connectionShouldBeEstablished() {
+		ResultSet result = DBConnector.makeQuery("SHOW TABLES;");
+		assertFalse(result.toString().equals(""));
+	};
 }
