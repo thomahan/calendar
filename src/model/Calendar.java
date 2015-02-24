7d91af483d8dda4bcca5aaacc71306bd08c12099
adrianisdahl <adrianti@stud.ntnu.no>
2015-02-24 10:36:24 +0100

    Calendar-class updated

--- src/model/Calendar.java
+++ src/model/Calendar.java
@@ -16,6 +16,7 @@ public class Calendar {
 	
 	public void addEvent(CalendarEvent event){
 		if (isValidEvent(event))events.add(event);
+		else throw new IllegalArgumentException("Unable to add event. Either you have an existing event at this time, or the event is already added. Please check your events ");
 
 	}
 	
@@ -24,9 +25,20 @@ public class Calendar {
 	}
 	
 	public boolean isValidEvent(CalendarEvent event){
+		if (events.contains(event)){
+			return false;
+		} else{
+			return true;
+		}
 		
+		// Ufullstendig metode. Må korrigere for tid (sjekke om avtale eksisterer på det gitte tidspunkt fra før)
 	}
 	
+	public void showMyEvents(){
+		for (CalendarEvent event: events){
+			System.out.println("Event name: " + event.eventName + ", Date: " + event.date + ", Room: " + event.room);
+		}
+	}
 	
 	
 	
