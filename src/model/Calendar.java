54d67c30c074be3b828f1a0b36505b606088b6e5
adrianisdahl <adrianti@stud.ntnu.no>
2015-02-24 10:01:43 +0100

    Oppdatert Calendar

--- src/model/Calendar.java
+++ src/model/Calendar.java
@@ -1,5 +1,35 @@
 package model;
 
+import java.util.ArrayList;
+
+
+
 public class Calendar {
 
+	private String calendarName;
+	private User owner;
+	private ArrayList<CalendarEvent> events = new ArrayList<CalendarEvent>();
+	
+	public Calendar(User owner){
+		this.owner = owner;
+	}
+	
+	public void addEvent(CalendarEvent event){
+		if (isValidEvent(event))events.add(event);
+
+	}
+	
+	public void removeEvent(CalendarEvent event){
+		events.remove(event);
+	}
+	
+	public boolean isValidEvent(CalendarEvent event){
+		
+	}
+	
+	
+	
+	
+	
+	
 }
