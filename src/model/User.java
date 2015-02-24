2fedc3e62fd78785684f799644eb9def65d69c8e
mikkellf <mikkelfuruberg@hotmail.com>
2015-02-24 10:42:51 +0100

    group og user endret

--- src/model/User.java
+++ src/model/User.java
@@ -1,5 +1,39 @@
 package model;
 
 public class User {
+	
+	private String name;
+	private final int employeeID;
+	private Calendar calendar;
+	
+	private User(String name, int employeeID) {
+		this.name = name;
+		this.employeeID = employeeID;
+	}
+	
+	private void changeUser(String newName) {
+		name = newName;
+	}
+	
+	private void createGroup(Group group) {
+		
+	}
 
+	public Calendar getCalendar() {
+		return calendar;
+	}
+
+	public void setCalendar(Calendar calendar) {
+		this.calendar = calendar;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getEmployeeID() {
+		return employeeID;
+	}
+	
+	
 }
