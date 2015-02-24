80474f9fe7eaf6e917e959209c0b2b6abcc677d6
Aksel Reiten <akselreiten@dhcp-067056.wlan.ntnu.no>
2015-02-24 09:45:23 +0100

    Added GroupListener and CalendarEventListener

--- src/model/CalendarEventListener.java
+++ src/model/CalendarEventListener.java
@@ -1,8 +1,5 @@
 package model;
 
-import java.sql.Date;
-import java.util.ArrayList;
-
 public interface CalendarEventListener {
 	
 	void eventHasChanged(CalendarEvent event);
--- src/model/GroupListener.java
+++ src/model/GroupListener.java
@@ -1,5 +1,7 @@
 package model;
 
 public interface GroupListener {
-
+	
+	void groupHasChanged(Group group);
+	
 }
