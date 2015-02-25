3509506da60e0f80d3b46cc8aa1793415ee82c13
adrianisdahl <adrianti@stud.ntnu.no>
2015-02-25 11:10:57 +0100

    Endring

--- src/model/Calendar.java
+++ src/model/Calendar.java
@@ -14,26 +14,28 @@ public class Calendar {
 		this.owner = owner;
 	}
 	
-	public void addEvent(CalendarEvent event){
+	public void addEvent(CalendarEvent event){ // Må også legges til i databasen
 		if (isValidEvent(event))events.add(event);
 		else throw new IllegalArgumentException("Unable to add event. Either you have an existing event at this time, or the event is already added. Please check your events ");
 
 	}
 	
 	public void removeEvent(CalendarEvent event){
-		events.remove(event);
+		events.remove(event); // Må fjernes fra databasen
 	}
 	
 	public boolean isValidEvent(CalendarEvent event){
-		if (events.contains(event)){
+		if (events.contains(event)){ // pluss sjekke databasen
 			return false;
 		}  else {
 			return true;
 		}
 		
-		// Ufullstendig metode. Må korrigere for tid (sjekke om avtale eksisterer på det gitte tidspunkt fra før). Det må gjøres når User-klasse er pushet
+		// Ufullstendig metode. Må korrigere for tid (sjekke om avtale eksisterer på det gitte tidspunkt fra før).
 		// For å se om brukeren har en event i det tidsrommet. 
 	}
+	
+
 	
 	public void showMyEvents(){
 		for (CalendarEvent event: events){
