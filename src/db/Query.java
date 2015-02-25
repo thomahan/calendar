540b204938535771853a49b971a30d77331a2c30
thomahan <thomasrenehansen@gmail.com>
2015-02-25 11:06:33 +0100

    Create class to handle query connections

---
+++ src/db/Query.java
@@ -0,0 +1,44 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Query {
+	Connection connection;
+	Statement statement;
+	ResultSet result;
+	
+	/**
+	 * Returns a Query object
+	 * @param connection
+	 * @param statement
+	 * @param result
+	 */
+	public Query(Connection connection, Statement statement, ResultSet result) {
+		this.connection = connection;
+		this.statement = statement;
+		this.result = result;
+	}
+	
+	/**
+	 * Returns result set
+	 * @return
+	 */
+	public ResultSet getResult() {
+		return result;
+	}
+	
+	/**
+	 * Closes connection and statement so the database won't run out of connections
+	 */
+	public void close() {
+		try {
+			statement.close();
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
