9611ff905426e9f6fc94a3a35344ef86a1bc7011
thomahan <thomasrenehansen@gmail.com>
2015-02-26 17:27:31 +0100

    Remove superfluous whitespace

--- test/db/DBConnectorTest.java
+++ test/db/DBConnectorTest.java
@@ -10,7 +10,6 @@ import org.junit.Test;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertNotNull;
 
-
 public class DBConnectorTest {
 	@Test
 	public void connectionShouldBeEstablished() {
