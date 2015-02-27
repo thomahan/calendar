a615e5cf14e6dc5eb0b9e593e51ad6c41d8b8af0
thomahan <thomasrenehansen@gmail.com>
2015-02-27 13:37:53 +0100

    Add test data

--- src/db/db_test_data_insertion.sql
+++ src/db/db_test_data_insertion.sql
@@ -1,2 +1,4 @@
-INSERT INTO user (firstName, lastName, username, salt, passwordHash)
-VALUES ('Per', 'Persen', 'per88', '091827306293846', '209387098649865987987');
+INSERT INTO user (first_name, last_name, username, salt, password_hash)
+VALUES ('Per', 'Persen', 'user1', '091827306293846', '209387098649865987987'),
+	('Ola', 'Olsen', 'user2', '293847928374923', '9238479238749238479283');
+
