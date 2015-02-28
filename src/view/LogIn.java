56304fc3dcbaa0decfa705181c913da9ace15fea
akselreiten <aksel.reiten@gmail.com>
2015-02-28 19:55:43 +0100

    New graphical layout in NewUser and LogIn-window.

--- src/view/LogIn.java
+++ src/view/LogIn.java
@@ -1,33 +1,25 @@
 package view;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JTextField;
-import javax.swing.JPasswordField;
 import javax.swing.JSplitPane;
-import javax.swing.JButton;
+import javax.swing.JDesktopPane;
 import javax.swing.JTextArea;
-import javax.swing.AbstractAction;
-
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-
+import javax.swing.AbstractAction;
 import javax.swing.Action;
 
-import java.awt.event.ActionListener;
+public class LogIn {
 
-import javax.swing.JFormattedTextField;
-import javax.swing.JList;
-
-public class LogIn extends JFrame {
-
-	private JPanel contentPane;
+	private JFrame frame;
+	private JTextField textField;
 	private JTextField textField_1;
 	private final Action action = new SwingAction();
-	private JPasswordField passwordField;
 	private final Action action_1 = new SwingAction_1();
 
 	/**
@@ -37,8 +29,8 @@ public class LogIn extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					LogIn frame = new LogIn();
-					frame.setVisible(true);
+					LogIn window = new LogIn();
+					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -47,77 +39,82 @@ public class LogIn extends JFrame {
 	}
 
 	/**
-	 * Create the frame.
+	 * Create the application.
 	 */
 	public LogIn() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
 		
 		JPanel panel = new JPanel();
-		contentPane.add(panel, BorderLayout.CENTER);
+		panel.setBounds(119, 67, 218, 133);
+		frame.getContentPane().add(panel);
 		panel.setLayout(null);
 		
 		JTextArea txtrUsername = new JTextArea();
-		txtrUsername.setBounds(26, 11, 62, 16);
+		txtrUsername.setBounds(5, 11, 62, 16);
 		txtrUsername.setText("Username");
 		panel.add(txtrUsername);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(141, 5, 134, 28);
-		panel.add(textField_1);
-		textField_1.setColumns(10);
+		JDesktopPane desktopPane = new JDesktopPane();
+		desktopPane.setBounds(72, 18, 1, 1);
+		panel.add(desktopPane);
+		
+		textField = new JTextField();
+		textField.setBounds(72, 5, 134, 28);
+		panel.add(textField);
+		textField.setColumns(10);
 		
 		JTextArea txtrPassword = new JTextArea();
-		txtrPassword.setBounds(26, 44, 59, 16);
+		txtrPassword.setBounds(10, 44, 59, 16);
 		txtrPassword.setText("Password");
 		panel.add(txtrPassword);
 		
-		passwordField = new JPasswordField();
-		passwordField.setBounds(141, 38, 134, 28);
-		panel.add(passwordField);
+		textField_1 = new JTextField();
+		textField_1.setBounds(72, 38, 134, 28);
+		panel.add(textField_1);
+		textField_1.setColumns(10);
 		
-		JButton btnLogIn = new JButton("Log in");
-		btnLogIn.setAction(action);
-		btnLogIn.setBounds(141, 78, 117, 29);
-		panel.add(btnLogIn);
-		
-		JTextArea txtrNotRegisteredYet = new JTextArea();
-		txtrNotRegisteredYet.setText("Not registered yet? Register by clicking the button.");
-		txtrNotRegisteredYet.setBounds(63, 172, 320, 21);
-		panel.add(txtrNotRegisteredYet);
-		
-		JButton btnNewUser = new JButton("New user");
-		btnNewUser.addActionListener(new ActionListener() {
+		JButton btnNewButton = new JButton("Confirm");
+		btnNewButton.setAction(action_1);
+		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnNewUser.setAction(action_1);
-		btnNewUser.setBounds(141, 205, 117, 29);
+		btnNewButton.setBounds(72, 75, 117, 29);
+		panel.add(btnNewButton);
+		
+		JButton btnNewUser = new JButton("New User");
+		btnNewUser.setAction(action);
+		btnNewUser.setBounds(72, 104, 117, 29);
 		panel.add(btnNewUser);
 	}
-
 	private class SwingAction extends AbstractAction {
 		public SwingAction() {
-			putValue(NAME, "Log in");
-			putValue(SHORT_DESCRIPTION, "Validates username and password and opens calendar if information is correct");
+			putValue(NAME, "New User");
+			putValue(SHORT_DESCRIPTION, "Some short description");
 		}
 		public void actionPerformed(ActionEvent e) {
-			CalendarWindow cp = new CalendarWindow();
-			cp.main(null);
+			NewUser nu = new NewUser();
+			nu.main(null);
 		}
 	}
 	private class SwingAction_1 extends AbstractAction {
 		public SwingAction_1() {
-			putValue(NAME, "New User");
-			putValue(SHORT_DESCRIPTION, "Opens window for new user");
+			putValue(NAME, "Confirm");
+			putValue(SHORT_DESCRIPTION, "Some short description");
 		}
 		public void actionPerformed(ActionEvent e) {
-			NewUser nu = new NewUser();
-			nu.setVisible(true);
+			CalendarWindow cw = new CalendarWindow();
+			cw.main(null);
 		}
 	}
 }
--- src/view/NewUser.java
+++ src/view/NewUser.java
@@ -1,36 +1,31 @@
 package view;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JSplitPane;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JButton;
-
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-
-import javax.swing.AbstractAction;
-import javax.swing.Action;
-import javax.swing.JTextArea;
-import javax.swing.JPasswordField;
+import java.awt.FlowLayout;
 import javax.swing.JTextField;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import java.awt.GridLayout;
+import java.awt.SystemColor;
+import javax.swing.JButton;
+import javax.swing.AbstractAction;
+import java.awt.event.ActionEvent;
+import javax.swing.Action;
 
-public class NewUser extends JFrame {
+public class NewUser {
 
-	private JPanel contentPane;
-	private final Action action = new SwingAction();
-	private JPasswordField passwordField;
-	private JPasswordField passwordField_1;
+	private JFrame frame;
 	private JTextField textField;
 	private JTextField textField_1;
 	private JTextField textField_2;
-	private JTextArea txtrPassword;
-	private JTextArea txtrConfirmPassword;
-	private JTextArea txtrLastName;
-	private JTextArea txtrGivenName;
-	private JTextArea txtrUsername;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JPanel panel_1;
+	private final Action action = new SwingAction();
 	private final Action action_1 = new SwingAction_1();
 
 	/**
@@ -40,8 +35,8 @@ public class NewUser extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					NewUser frame = new NewUser();
-					frame.setVisible(true);
+					NewUser window = new NewUser();
+					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -50,106 +45,120 @@ public class NewUser extends JFrame {
 	}
 
 	/**
-	 * Create the frame.
+	 * Create the application.
 	 */
 	public NewUser() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		
-		JButton btnNewButton = new JButton("Confirm information");
-		btnNewButton.setBounds(52, 210, 159, 38);
-		btnNewButton.setAction(action);
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		contentPane.setLayout(null);
-		contentPane.add(btnNewButton);
-		
-		JTextArea txtrPleaseFillInn = new JTextArea();
-		txtrPleaseFillInn.setText("Please fill inn correct information");
-		txtrPleaseFillInn.setBounds(116, 6, 210, 16);
-		contentPane.add(txtrPleaseFillInn);
-		
-		JButton btnExit = new JButton("Cancel");
-		btnExit.setAction(action_1);
-		btnExit.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnExit.setBounds(222, 210, 178, 38);
-		contentPane.add(btnExit);
-		
-		passwordField = new JPasswordField();
-		passwordField.setBounds(142, 129, 180, 23);
-		contentPane.add(passwordField);
-		
-		passwordField_1 = new JPasswordField();
-		passwordField_1.setBounds(142, 156, 178, 22);
-		contentPane.add(passwordField_1);
-		
-		textField = new JTextField("");
-		textField.setBounds(140, 100, 180, 23);
-		contentPane.add(textField);
-		textField.setColumns(10);
-		
-		textField_1 = new JTextField("");
-		textField_1.setBounds(140, 75, 180, 23);
-		contentPane.add(textField_1);
-		textField_1.setColumns(10);
-		
-		textField_2 = new JTextField("");
-		textField_2.setBounds(140, 50, 180, 23);
-		contentPane.add(textField_2);
-		textField_2.setColumns(10);
-		
-		txtrPassword = new JTextArea();
-		txtrPassword.setText("Password");
-		txtrPassword.setBounds(65, 133, 65, 16);
-		contentPane.add(txtrPassword);
-		
-		txtrConfirmPassword = new JTextArea();
-		txtrConfirmPassword.setText("Confirm password");
-		txtrConfirmPassword.setBounds(10, 159, 120, 16);
-		contentPane.add(txtrConfirmPassword);
-		
-		txtrLastName = new JTextArea();
-		txtrLastName.setText("Last name");
-		txtrLastName.setBounds(63, 104, 65, 19);
-		contentPane.add(txtrLastName);
-		
-		txtrGivenName = new JTextArea();
-		txtrGivenName.setText("Given name");
-		txtrGivenName.setBounds(52, 79, 78, 19);
-		contentPane.add(txtrGivenName);
-		
-		txtrUsername = new JTextArea();
-		txtrUsername.setText("Username");
-		txtrUsername.setBounds(59, 54, 65, 16);
-		contentPane.add(txtrUsername);
+		initialize();
 	}
 
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(186, 54, 154, 173);
+		frame.getContentPane().add(panel);
+		panel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		textField = new JTextField();
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		panel.add(textField_1);
+		textField_1.setColumns(10);
+		
+		textField_2 = new JTextField();
+		panel.add(textField_2);
+		textField_2.setColumns(10);
+		
+		textField_3 = new JTextField();
+		panel.add(textField_3);
+		textField_3.setColumns(10);
+		
+		textField_4 = new JTextField();
+		panel.add(textField_4);
+		textField_4.setColumns(10);
+		
+		panel_1 = new JPanel();
+		panel_1.setBounds(96, 6, 244, 38);
+		frame.getContentPane().add(panel_1);
+		panel_1.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		JTextArea txtrPleaseFillIn = new JTextArea();
+		txtrPleaseFillIn.setText("Please fill in required information");
+		panel_1.add(txtrPleaseFillIn);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBounds(69, 54, 123, 173);
+		frame.getContentPane().add(panel_2);
+		panel_2.setLayout(new GridLayout(0, 1, 0, 0));
+		
+		JTextArea txtrUsername = new JTextArea();
+		txtrUsername.setLineWrap(true);
+		txtrUsername.setBackground(SystemColor.window);
+		txtrUsername.setText("Username");
+		panel_2.add(txtrUsername);
+		
+		JTextArea txtrGivenName = new JTextArea();
+		txtrGivenName.setLineWrap(true);
+		txtrGivenName.setBackground(SystemColor.window);
+		txtrGivenName.setText("Given name");
+		panel_2.add(txtrGivenName);
+		
+		JTextArea txtrLastName = new JTextArea();
+		txtrLastName.setLineWrap(true);
+		txtrLastName.setBackground(SystemColor.window);
+		txtrLastName.setText("Last name");
+		panel_2.add(txtrLastName);
+		
+		JTextArea txtrPassword = new JTextArea();
+		txtrPassword.setLineWrap(true);
+		txtrPassword.setBackground(SystemColor.window);
+		txtrPassword.setText("Password");
+		panel_2.add(txtrPassword);
+		
+		JTextArea txtrConfirmPassword = new JTextArea();
+		txtrConfirmPassword.setLineWrap(true);
+		txtrConfirmPassword.setBackground(SystemColor.window);
+		txtrConfirmPassword.setText("Confirm password");
+		panel_2.add(txtrConfirmPassword);
+		
+		JPanel panel_3 = new JPanel();
+		panel_3.setBounds(124, 236, 273, 36);
+		frame.getContentPane().add(panel_3);
+		panel_3.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		JButton btnConfirm = new JButton("Confirm");
+		btnConfirm.setAction(action);
+		panel_3.add(btnConfirm);
+		
+		JButton btnCancel = new JButton("Cancel");
+		btnCancel.setAction(action_1);
+		panel_3.add(btnCancel);
+	}
 	private class SwingAction extends AbstractAction {
 		public SwingAction() {
-			putValue(NAME, "Confirm information");
-			putValue(SHORT_DESCRIPTION, "By pressing this button you confirm information, validate information and proceeds to the calendarwindow if everything is OK.");
+			putValue(NAME, "Confirm");
+			putValue(SHORT_DESCRIPTION, "Some short description");
 		}
 		public void actionPerformed(ActionEvent e) {
-			CalendarWindow cp = new CalendarWindow();
-			cp.main(null);
+			CalendarWindow cw = new CalendarWindow();
+			cw.main(null);
 		}
 	}
 	private class SwingAction_1 extends AbstractAction {
 		public SwingAction_1() {
-			putValue(NAME, "Cancel");
-		putValue(SHORT_DESCRIPTION, "Quits the process and returns to loginwindow");
+			putValue(NAME, "Quit");
+			putValue(SHORT_DESCRIPTION, "Some short description");
 		}
 		public void actionPerformed(ActionEvent e) {
 			System.exit(0);
-			
 		}
 	}
 }
