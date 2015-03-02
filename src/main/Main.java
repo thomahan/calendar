c5ce74597f16d4492e5c6c3dff9bbbd1050dbce2
thomahan <thomasrenehansen@gmail.com>
2015-03-02 16:29:00 +0100

    Refactor Controller call after argument removed

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,8 +4,7 @@ import view.LogIn;
 
 public class Main {
 	public static void main(String[] args) {
-		LogIn loginView = new LogIn();
-		Controller controller = new Controller(loginView);
+		Controller controller = new Controller();
 	}
 
 }
