618da769216dd4323e223648ae6e93ec18e6c85f
thomahan <thomasrenehansen@gmail.com>
2015-03-03 13:37:54 +0100

    Add suppress warning for unused

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,8 @@
 package main;
 
-import view.LogIn;
-
 public class Main {
 	public static void main(String[] args) {
+		@SuppressWarnings("unused")
 		Controller controller = new Controller();
 	}
 
