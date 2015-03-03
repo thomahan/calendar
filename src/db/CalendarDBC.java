cdfb8ad9fe2c1d6f53c1b8bdd0f75c7d6e054972
adrianisdahl <adrianti@stud.ntnu.no>
2015-03-03 13:28:20 +0100

    CalendarDBC

--- src/db/CalendarDBC.java
+++ src/db/CalendarDBC.java
@@ -1,5 +1,30 @@
 package db;
 
-public class CalendarDBC {
+import java.sql.ResultSet;
 
+import model.Calendar;
+import model.CalendarEvent;
+import model.User;
+
+public class CalendarDBC {
+	
+	
+	
+
+	public static void addEvent(String username, CalendarEvent event){
+		
+		
+		
+	}
+	
+	public static void removeEvent(String username, CalendarEvent event){
+		
+		
+	}
+	
+	public static Calendar getCalendar(String username){
+		
+	}
+	
+	
 }
