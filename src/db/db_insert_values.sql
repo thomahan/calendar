5ddc71e5bba437e44fd759668c91bb6c6806a8b2
thomahan <thomasrenehansen@gmail.com>
2015-03-03 13:35:18 +0100

    Removed content, not needed yet.

--- src/db/db_insert_values.sql
+++ src/db/db_insert_values.sql
@@ -1,4 +1,1 @@
-INSERT INTO user (first_name, last_name, username, salt, password_hash)
-VALUES ('Per', 'Persen', 'user1', '091827306293846', '209387098649865987987'),
-	('Ola', 'Olsen', 'user2', '293847928374923', '9238479238749238479283');
 
