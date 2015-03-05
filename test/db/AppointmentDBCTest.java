f83adf951df520b030c05000150b2cfef879ac70
thomahan <thomasrenehansen@gmail.com>
2015-03-05 21:16:40 +0100

    Add argument after change in AppointmentDBC

--- test/db/AppointmentDBCTest.java
+++ test/db/AppointmentDBCTest.java
@@ -1,28 +1,29 @@
 package db;
 
+import java.util.Date;
+
 import org.junit.Test;
-import static org.junit.Assert.assertTrue;
 
 public class AppointmentDBCTest {
 	@SuppressWarnings("deprecation")
 	
-/*
 	@Test
 	public void appointmentShouldBeAddedToDatabase() {
 		Date startTime = new Date(2015-1900, 4, 8, 16, 15, 00);
 		Date endTime = new Date(2015-1900, 4, 8, 18, 00, 00);
 		Date alarmTime = new Date(2015-1900, 4, 8, 16, 00, 00);
-		String description = "Sage";
+		String title = "Sage ved";
+		String description = "Da var det vel på tide å sage ved igjen?";
 		String location = "Bua";
-		String username = "user1";
+		String username = "user";
 		int roomId = 1;
 		
-		CalendarDBC.addEvent(startTime, endTime, alarmTime, description, location, username, roomId);
+		AppointmentDBC.addAppointment(startTime, endTime, alarmTime, title, description, location, username, roomId);
 	}
-*/
-	
+/*
 	@Test
 	public void addInvitationShouldInviteUserToAppointment() {
 		AppointmentDBC.addInvitation(11, "user1", null);
 	}
+	*/
 }
