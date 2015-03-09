7a3175e398117823e1a4264f10233b5a08fc2725
thomahan <thomasrenehansen@gmail.com>
2015-03-09 17:54:11 +0100

    Simplify instantiation

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,8 +2,6 @@ package main;
 
 public class Main {
 	public static void main(String[] args) {
-		@SuppressWarnings("unused")
-		Controller controller = new Controller();
+		new Controller();
 	}
-
 }
