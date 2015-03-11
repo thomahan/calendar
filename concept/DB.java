b5cdbe817b18a35e1cd40d1640d6c3bfe877da17
thomahan <thomasrenehansen@gmail.com>
2015-03-11 10:28:15 +0100

    Add concept again

---
+++ concept/DB.java
@@ -0,0 +1,17 @@
+
+public class DB {
+	private int value;
+	
+	public DB() {
+		this.value = 0;
+	}
+
+	public int getValue() {
+		return value;
+	}
+	
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+}
