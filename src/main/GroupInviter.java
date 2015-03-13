2594c4728c2cea00ee9c81882f547a3e0e0958c0
thomahan <thomasrenehansen@gmail.com>
2015-03-13 16:53:45 +0100

    Add logic for adding all subgroups and members

--- src/main/GroupInviter.java
+++ src/main/GroupInviter.java
@@ -1,19 +1,53 @@
 package main;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import model.Group;
+import model.User;
 import db.AppointmentDBC;
+import db.UserDBC;
 
 public class GroupInviter {
 	public static void inviteGroupList(int appointmentId, List<Group> groupList) {
-		for (Group g : groupList) {
-			System.out.println(g);
-		}
-		//groupList.addAll(AppointmentDBC.getSubGroupList(groupList));
-		
-	
-		
-	}
+		// Expand groups into subgroups
+		groupList = getAllSubGroupList(groupList);
 
+		// Invite all groups and subgroups
+		for (Group group : groupList) {
+			AppointmentDBC.addGroupInvitation(appointmentId, group.getGroupID());
+		}
+
+		List<User> invitedUserList = new ArrayList<User>();
+		for (Group group : groupList) {
+			List<User> userList = UserDBC.getMemberList(group.getGroupID());
+			for (User user : userList) {
+				if (!invitedUserList.contains(user)) {
+					invitedUserList.add(user);
+				}
+			}
+		}
+
+	
+		for (User user : invitedUserList) {
+			AppointmentDBC.addInvitation(appointmentId, user.getUsername(), "Not replied");
+		}
+}
+	
+	public static List<Group> getAllSubGroupList(List<Group> groupList) {
+		List<Group> subGroupList = new ArrayList<Group>();
+		
+		for (Group group : groupList) {
+			List<Group> gList = UserDBC.getSubGroupList(group.getGroupID());
+			gList = getAllSubGroupList(gList);
+			for (Group g : gList) {
+				if (!groupList.contains(g)) {
+					subGroupList.add(g);
+				}
+			}
+		}
+
+		groupList.addAll(subGroupList);
+		return groupList;
+	}
 }
