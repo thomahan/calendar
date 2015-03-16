15088de867c67466facb1d26c556728d02d742f9
thomahan <thomasrenehansen@gmail.com>
2015-03-16 18:15:16 +0100

    Change after recommendations from student assintant

--- src/db/db_create_tables.sql
+++ src/db/db_create_tables.sql
@@ -2,20 +2,20 @@ CREATE TABLE IF NOT EXISTS user (
 	username VARCHAR(64) NOT NULL,
 	salt VARCHAR(128) NOT NULL,
 	password_hash VARCHAR(128) NOT NULL,
-	first_name VARCHAR(256),
-	last_name VARCHAR(256),
+	first_name VARCHAR(20),
+	last_name VARCHAR(30),
 	PRIMARY KEY (username)
 ) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS user_group (
 	user_group_id INT NOT NULL AUTO_INCREMENT,
-	name VARCHAR(256),
+	name VARCHAR(20),
 	PRIMARY KEY (user_group_id)
 ) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS room (
 	room_id INT NOT NULL AUTO_INCREMENT,
-	name VARCHAR(256),
+	name VARCHAR(20),
 	seat_count INT NOT NULL,
 	PRIMARY KEY (room_id)
 ) ENGINE=InnoDB;
@@ -24,19 +24,19 @@ CREATE TABLE IF NOT EXISTS appointment (
 	appointment_id INT NOT NULL AUTO_INCREMENT,
 	start_time DATETIME NOT NULL,
 	end_time DATETIME NOT NULL,
-	description VARCHAR(256) NOT NULL,
-	location VARCHAR(256),
+	description VARCHAR(50) NOT NULL,
+	location VARCHAR(50),
 	room_id INT,
 	creator VARCHAR(64) NOT NULL,
 	PRIMARY KEY (appointment_id),
 	CONSTRAINT FOREIGN KEY (creator) REFERENCES user(username) ON UPDATE CASCADE ON DELETE CASCADE,
-	CONSTRAINT FOREIGN KEY (room_id) REFERENCES room(room_id) ON UPDATE CASCADE ON DELETE CASCADE
+	CONSTRAINT FOREIGN KEY (room_id) REFERENCES room(room_id) ON UPDATE CASCADE ON DELETE SET NULL
 ) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS invitation (
 	appointment_id INT NOT NULL,
 	username VARCHAR(64) NOT NULL,
-	status VARCHAR(32) DEFAULT "Not replied",
+	status VARCHAR(20) DEFAULT "Not replied",
 	unseen_change BOOL DEFAULT 0,
 	alarm_time DATETIME,
 	PRIMARY KEY (appointment_id, username),
